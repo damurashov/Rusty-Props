6ec8c65a3e90a7d8a8a832945448180bb8152861
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-18 17:48:21 +0300

    [src:algorithm] Bug | Misconceived structure

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,7 +1,7 @@
 /// Neural network activation and training algorithms.
 ///
 
-use network;
+use crate::network;
 use std::assert;
 
 pub type Signal = std::vec::Vec<f32>;
@@ -12,7 +12,7 @@ pub struct Prop {
 
 impl Prop {
 	fn network_init(net: &mut network::Network, input: &Signal) {
-		net.layers[0].a = input;
+		net.init_input_layer(input);
 	}
 
 	fn network_update_layer(&self, net: &mut network::Network, ilayer: usize) {
@@ -21,23 +21,24 @@ impl Prop {
 			let mut sum = 0.0f32;
 
 			// W = Ax + b, W - vec. of weights, A - vec. of activation function values, b - vector of biases
-			for i_prev in 0..layer_len(ilayer - 1) {
-				let edge = net.edge(ilayer, i_prev, i)
+			for i_prev in 0..net.layer_len(ilayer - 1) {
+				let edge = net.edge(ilayer, i_prev, i);
 				sum += edge.w * net.a(ilayer - 1, i_prev) + edge.b;
 			}
 		}
 	}
 
-	pub fn run(&self, net: &mut network::Network) {
+	pub fn run(&self, net: &mut network::Network, input: &Signal) {
 		Prop::network_init(net, input);
 
 		for ilayer in 1..net.n_layers() {
-			self.network_update_layer(network, ilayer);
+			self.network_update_layer(net, ilayer);
 		}
 	}
 }
 
-pub fn network_prop(net: &mut network::Network, input: &Signal) {
-	for ilayer in 0..network.n_layers {
-	}
+#[cfg(test)]
+mod tests {
+//	#[test]
+//	fn
 }
