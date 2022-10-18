96f33f27e0da799543d56540101923beaaa8cef4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-18 18:10:33 +0300

    [src:algorithm] Impl | Forward propagation

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -15,16 +15,19 @@ impl Prop {
 		net.init_input_layer(input);
 	}
 
-	fn network_update_layer(&self, net: &mut network::Network, ilayer: usize) {
+	/// Forward propagation between adjacent layers
+	fn network_update_layer_propagate(&self, net: &mut network::Network, ilayer: usize) {
 		assert!(ilayer > 0);
-		for i in 0..net.layer_len(ilayer) {
+
+		for ito in 1..net.layer_len(ilayer) {
 			let mut sum = 0.0f32;
 
-			// W = Ax + b, W - vec. of weights, A - vec. of activation function values, b - vector of biases
-			for i_prev in 0..net.layer_len(ilayer - 1) {
-				let edge = net.edge(ilayer, i_prev, i);
-				sum += edge.w * net.a(ilayer - 1, i_prev) + edge.b;
+			for ifrom in 1..net.layer_len(ilayer - 1) {
+				let edge = net.edge(ilayer, ifrom, ito);
+				sum += net.a(ilayer - 1, ifrom) * edge.w + edge.b;
 			}
+
+			net.set_z(ilayer, ito, sum);
 		}
 	}
 
@@ -32,7 +35,7 @@ impl Prop {
 		Prop::network_init(net, input);
 
 		for ilayer in 1..net.n_layers() {
-			self.network_update_layer(net, ilayer);
+			self.network_update_layer_propagate(net, ilayer);
 		}
 	}
 }
