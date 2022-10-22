09c86de13b0dd536fe9796d8b25c3e13c636fbe8
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-22 14:26:32 +0300

    [src:algorithm] Ref | Comments

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -118,6 +118,15 @@ struct BackPropagation {
 }
 
 impl BackPropagation {
+	// TODO dcdz
+	// TODO dcda
+	// TODO dcdw
+	// TODO dcdb
+	// TODO dzdw
+	// TODO dzdb
+	// TODO dzda
+	// TODO dadz
+
 	fn from_network(net: &network::Network, dcdz_output: Dcdz, dadz: Dadz) -> BackPropagation {
 		let geometry: Vec<usize> = (0..net.n_layers()).map(|i| net.layer_len(i)).collect();
 		BackPropagation {
