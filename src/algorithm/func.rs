0fdd333f0cd6abf4cc7045b4df4a4cf7cfe9bc95
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-25 15:54:36 +0300

    [algorithm:func] Impl | MSE cost function, derivative

--- src/algorithm/func.rs
+++ src/algorithm/func.rs
@@ -16,3 +16,11 @@ pub fn activation_step_d(z: f32) -> f32 {
 		1.0
 	}
 }
+
+/// Mean squared error fucntion's derivative
+///
+/// `reference` - desired output, training value
+/// `value` - factual output
+pub fn cost_mse_d(reference: f32, value: f32) -> f32 {
+	-2.0f32 * reference + 2.0f32 * value
+}
