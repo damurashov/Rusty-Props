8f497aba29d4684fa1dd9bd1488d862628690546
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-05 17:41:02 +0300

    [:build] Build | Test data unpacking (de-gzipping)

--- build.rs
+++ build.rs
@@ -1,7 +1,25 @@
-use std::{error::Error, env::current_dir, path::{Path, PathBuf}, fs::File, fs::create_dir, io::Write, process::Command};
+use std::{error::Error,
+	env::current_dir,
+	path::{
+		Path,
+		PathBuf
+	},
+	fs:: {
+		File,
+		create_dir,
+	},
+	io::{
+		Write,
+		BufReader,
+		BufWriter,
+		copy
+	},
+	process::Command
+};
 use tokio;
 use reqwest;
 use lazy_static::lazy_static;
+use libflate;
 
 mod dataset {
 	use super::*;
@@ -11,6 +29,13 @@ mod dataset {
 		"http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
 		"http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
 		"http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz",
+	];
+
+	static MNIST_FILES: &[&str] = &[
+		"train-images-idx3-ubyte",
+		"train-labels-idx1-ubyte",
+		"t10k-images-idx3-ubyte",
+		"t10k-labels-idx1-ubyte",
 	];
 
 	lazy_static! {
@@ -34,11 +59,30 @@ mod dataset {
 
 		Ok(())
 	}
+
+	/// Deflates `gzip`s
+	///
+	/// Pre: `data/` directory must exist
+	pub fn unpack() {
+		for fname in MNIST_FILES {
+			let mut path_out= PATH_BASE.clone();
+			path_out.push(fname);
+			let mut path_in = path_out.clone();
+			path_in.set_extension("gz");
+			let mut file_in = File::open(&path_in).unwrap();
+			let mut file_out = File::create(&path_out).unwrap();
+			let mut stream_in = BufReader::new(&file_in);
+			let mut stream_out = BufWriter::new(&mut file_out);
+			let mut gz_decoder = libflate::gzip::Decoder::new(&mut stream_in).unwrap();
+			copy(&mut gz_decoder, &mut stream_out);
+		}
+	}
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let mut rt = tokio::runtime::Runtime::new().unwrap();
 	rt.block_on(async {dataset::fetch().await})?;
+	dataset::unpack();
 
 	Ok(())
 }
