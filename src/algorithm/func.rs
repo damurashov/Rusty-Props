cf1c1b86bea9bc0b14c609f3b67028809bdb30a5
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-06 19:11:25 +0300

    [src:func] Ref | Spacing: tabs vs. spaces. Switched to tabs, as per the standard

--- src/algorithm/func.rs
+++ src/algorithm/func.rs
@@ -1,20 +1,20 @@
 
 /// Step function
 pub fn activation_step(z: f32) -> f32 {
-	if z < 0.0 {
-		0.0
-	} else {
-		z
-	}
+    if z < 0.0 {
+        0.0
+    } else {
+        z
+    }
 }
 
 /// da/dz
 pub fn activation_step_d(z: f32) -> f32 {
-	if z < 0.0 {
-		0.0
-	} else {
-		1.0
-	}
+    if z < 0.0 {
+        0.0
+    } else {
+        1.0
+    }
 }
 
 /// Mean squared error fucntion's derivative
@@ -22,5 +22,5 @@ pub fn activation_step_d(z: f32) -> f32 {
 /// `reference` - desired output, training value
 /// `value` - factual output
 pub fn cost_mse_d(reference: f32, value: f32) -> f32 {
-	-2.0f32 * reference + 2.0f32 * value
+    -2.0f32 * reference + 2.0f32 * value
 }
