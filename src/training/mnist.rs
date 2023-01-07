49778e9513177c294f285b9d9b9d5bdc03369c23
DmitryMurashov <dmtr.murashov@gmail.com>
2023-01-07 21:12:58 +0300

    [training:mnist] Impl | Mnist, initials (carcass)

--- src/training/mnist.rs
+++ src/training/mnist.rs
@@ -4,6 +4,26 @@
 /// This module trains the network using this dataset.
 
 use mnist::{Mnist, MnistBuilder};
+use crate::algorithm;
+use crate::network;
+use crate::ut;
+
+const IMG_SIZE: usize = 28;  // Handwritten digits, 28x28
+const N_CLASSES: usize = 10;  // 0-9
+
+fn mnist_load(training_set_length: usize, test_set_length: usize) -> Mnist {
+    use std::env::current_dir;
+
+    let mut path_base = current_dir().unwrap();
+    path_base.push("data");
+    let path_base_str = path_base.as_path().to_str().unwrap();
+
+    MnistBuilder::new().label_format_digit()
+        .training_set_length(training_set_length.try_into().unwrap())
+        .test_set_length(test_set_length.try_into().unwrap())
+        .base_path(path_base_str)
+        .finalize()
+}
 
 /// Waterprobing. An attempt to load and unpack MNIST dataset
 #[cfg(test)]
@@ -36,5 +56,37 @@ mod test_mnist_load {
         assert!(trn_lbl.len() == TRAINING_SET_LEN);
         assert!(tst_img.len() == IMG_SIZE * TEST_SET_LEN);
         assert!(tst_lbl.len() == TEST_SET_LEN);
+    }
+}
+
+fn train(net: &mut network::Network, mnist: &Mnist, ibegin_training_image: usize) {
+}
+
+fn test(net: &mut network::Network, mnist: &Mnist) {
+}
+
+pub fn main() {
+    use std::env::args;
+
+    const TRAINING_SET_LEN: usize = 2000;
+    const TEST_SET_LEN: usize = 100;
+    const NETWORK_GEOMETRY: [usize; 4] = [IMG_SIZE, 16, 8, 10];
+    const NETWORK_FILE: &str = "network.bin";
+    let args: Vec<String> = args().collect();
+    let mut ibegin_img = 0i32;
+    let mut network = {
+        if args.len() == 1 {
+            network::Network::from_geometry(&NETWORK_GEOMETRY.into())
+        } else {
+            ibegin_img = args[1].parse::<i32>().unwrap();
+            ut::network_deserialize_from_file(NETWORK_FILE).unwrap()
+        }
+    };
+    let mnist = mnist_load(TRAINING_SET_LEN, TEST_SET_LEN);
+
+    if ibegin_img < 0 {
+        test(&mut network, &mnist);
+    } else {
+        train(&mut network, &mnist, ibegin_img as usize);
     }
 }
