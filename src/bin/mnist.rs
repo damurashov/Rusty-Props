6269bd93eea5fd6b82aad73c1980d63c6603a315
DmitryMurashov <dmtr.murashov@gmail.com>
2023-03-19 10:42:49 +0300

    [bin:mnist] Enh | Enhanced `MnistWrapper`, implemented backprop invocation
    - Extended `MnistWrapper`, so it is able to trace the training state now;
    - Implemented call to `rusty_props::algorithm::<backprop wrapper function >`

--- src/bin/mnist.rs
+++ src/bin/mnist.rs
@@ -13,33 +13,42 @@ const OUTPUT_NEURONS_NUMBER: usize = 10;
 const NETWORK_GEOMETRY: [usize; 4] = [IMG_SIZE_BYTES, 16, 8, OUTPUT_NEURONS_NUMBER];
 const MNIST_OUTPUT_LAYER_SIZE: usize = 10;  // Mnist is a handwritten digits annotated database, 10 digits
 
-/// Satisfies trait/crate definition requirements
-struct MnistWrapper(mnist::Mnist);
+/// Encapsulates traininig state, so it can be resumed later
+struct MnistTrainingState<'a> {
+    dataset: &'a mnist::Mnist,
+    base_offset: usize,
+}
+
+impl MnistTrainingState<'_> {
+    fn offset_as_relative(&self, offset: usize) -> usize {
+        return offset + self.base_offset;
+    }
+}
 
 /// Implements signal initialization for MNIST dataset.
 ///
 /// MNIST dataset is an annotated dataset of handwritten digits.
 /// More on that here: https://en.wikipedia.org/wiki/MNIST_database
-impl ut::data::Dataset for MnistWrapper {
+impl ut::data::Dataset for MnistTrainingState<'_> {
     fn copy_training_input_signal(&self, image_index: usize,
             signal: &mut ut::data::Signal) {
-        let start_position = image_index * IMG_SIZE_BYTES;
+        let start_position = (self.offset_as_relative(image_index)) * IMG_SIZE_BYTES;
         <[u8] as ut::data::CopyConvertIntoSignal>::copy_convert_into_signal(
-            &self.0.trn_img[start_position..start_position + IMG_SIZE_BYTES],
+            &self.dataset.trn_img[start_position..start_position + IMG_SIZE_BYTES],
             signal
         );
     }
 
     fn copy_training_output_signal(&self, image_index: usize,
             signal: &mut ut::data::Signal) {
-        let position = self.0.trn_lbl[image_index] as usize;
+        let position = self.dataset.trn_lbl[self.offset_as_relative(image_index)] as usize;
         signal.reserve_exact(MNIST_OUTPUT_LAYER_SIZE);
         signal.resize(MNIST_OUTPUT_LAYER_SIZE, 0.0f32);
         signal[position] = 1.0;
     }
 
     fn length(&self) -> usize {
-        self.0.trn_lbl.len()
+        self.dataset.trn_lbl.len() - self.base_offset
     }
 }
 
@@ -96,6 +105,20 @@ mod test_mnist_load {
 /// Trains network using back propagation algorithm. It can resume previously
 /// started training session, if ibegin_training_image > 0
 fn train_network(net: &mut network::Network, mnist: &Mnist, ibegin_training_image: usize) {
+    let mut mnist_training_state = MnistTrainingState{
+        dataset: mnist,
+        base_offset: ibegin_training_image,
+    };
+    let activation_function_family = algorithm::ActivationFunctionFamily::StepFunction;
+    let cost_function = algorithm::func::cost_mse_d;
+    let training_rate = 0.001f32;
+    algorithm::train_network_back_propagation(net, activation_function_family,
+        cost_function, training_rate, &mnist_training_state,
+        |iteration_number| {
+            println!("{} of {}",
+                iteration_number,
+                (&mnist_training_state as &dyn ut::data::Dataset).length());
+        });
 }
 
 /// Runs forward propagation on a network, measures its performance.
