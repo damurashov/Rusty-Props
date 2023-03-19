90a84c79aaa3568a3105d900b243653b5cd0dca8
DmitryMurashov <dmtr.murashov@gmail.com>
2023-03-19 11:37:24 +0300

    [algorithm:func] Ref | Redundancies

--- src/algorithm/func.rs
+++ src/algorithm/func.rs
@@ -27,10 +27,10 @@ pub fn cost_mse_d(reference: f32, value: f32) -> f32 {
 }
 
 pub fn sum_squared_errors_cost_function(reference: &Signal, value: &Signal) -> f32 {
-    use std::iter::zip;
     assert!(reference.len() == value.len());
-    let mut sum = 0.0f32;
     reference.iter()
         .zip(value.iter())
-        .fold(0.0f32, |accumulated, (a, b)| accumulated + (a + b).powf(2.0))
+        .fold(0.0f32, |accumulated, (a, b)| {
+            accumulated + (a + b).powf(2.0)
+        })
 }
