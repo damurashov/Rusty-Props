dd3945ad337bd7d6689a7fbef4b9d2b5866446fa
DmitryMurashov <dmtr.murashov@gmail.com>
2023-03-19 09:35:11 +0300

    [ut:data] Ref | Moved MNIST-related functionality into binaries

--- src/ut/data.rs
+++ src/ut/data.rs
@@ -1,10 +1,6 @@
-pub use mnist;
 use core::ops::Index;
 
 pub type Signal = std::vec::Vec<f32>;
-
-const MNIST_IMAGE_SIZE: usize = 28 * 28;
-const MNIST_OUTPUT_LAYER_SIZE: usize = 10;  // Mnist is a handwritten digits annotated database, 10 digits
 
 /// `M` - metadatata (label) type, annotation associated w/ the payload
 ///
@@ -29,7 +25,7 @@ pub trait Dataset
 
 /// Helper trait for quickly initializing `Signal` instances from various types'
 /// instances
-trait CopyConvertIntoSignal {
+pub trait CopyConvertIntoSignal {
     fn copy_convert_into_signal(&self, signal: &mut Signal);
 }
 
@@ -58,26 +54,5 @@ where
 impl<T> Length for [T] {
     fn length(&self) -> usize {
         self.len()
-    }
-}
-
-/// Implements signal initialization for MNIST dataset.
-///
-/// MNIST dataset is an annotated dataset of handwritten digits.
-/// More on that here: https://en.wikipedia.org/wiki/MNIST_database
-impl Dataset for mnist::Mnist {
-    fn copy_training_input_signal(&self, image_index: usize,
-            signal: &mut Signal) {
-        let start_position = image_index * MNIST_IMAGE_SIZE;
-        self.trn_img[start_position..start_position + MNIST_IMAGE_SIZE]
-            .copy_convert_into_signal(signal);
-    }
-
-    fn copy_training_output_signal(&self, image_index: usize,
-            signal: &mut Signal) {
-        let position = self.trn_lbl[image_index] as usize;
-        signal.reserve_exact(MNIST_OUTPUT_LAYER_SIZE);
-        signal.resize(MNIST_OUTPUT_LAYER_SIZE, 0.0f32);
-        signal[position] = 1.0;
     }
 }
