5ca2cf80a2cbee510f5384abcf06fe1d604ac489
DmitryMurashov <dmtr.murashov@gmail.com>
2023-06-14 16:31:38 +0300

    [algorithm:func] Impl | Logging

--- src/algorithm/func.rs
+++ src/algorithm/func.rs
@@ -30,7 +30,9 @@ pub fn cost_mse_d(reference: f32, value: f32) -> f32 {
 }
 
 pub fn sum_squared_errors_vector_cost_function(reference: &Signal, value: &Signal) -> f32 {
+    use log;
     assert!(reference.len() == value.len());
+    log::trace!("Reference signal: {:?}, provided signal: {:?}", &reference, &value);
     reference.iter()
         .zip(value.iter())
         .fold(0.0f32, |accumulated, (a, b)| {
